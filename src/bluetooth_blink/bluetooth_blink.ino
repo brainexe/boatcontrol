84f7484f55146bc9add685f58bc4d0359acfa0f0
matze <matze@mdoetsch.de>
2014-12-26 17:32:58 +0100

    Serial -> BTSerial is working

--- src/bluetooth_blink/bluetooth_blink.ino
+++ src/bluetooth_blink/bluetooth_blink.ino
@@ -4,22 +4,32 @@ char btVal;
 
 int ledpin = 13;
 int enPin  = 5;
+int rxPin  = 15;
+int txPin  = 14;
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(15, 14); // RX | TX
+SoftwareSerial BTSerial(rxPin, txPin); // RX | TX
 
 void setup() {
   pinMode(ledpin, OUTPUT);
-  pinMode(enPin, OUTPUT);
   
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  
+  // set EN pin
+  pinMode(enPin, OUTPUT);
   digitalWrite(enPin, HIGH);
   
+  /*
   BTSerial.begin(115200);  // The Bluetooth Mate defaults to 115200bps
   BTSerial.print("$");  // Print three times individually
   BTSerial.print("$");
   BTSerial.print("$");  // Enter command mode
+  */
+  
   BTSerial.begin(9600);
+  
   Serial.begin(115200);
 }
  
@@ -27,35 +37,26 @@ void loop() {
   val   = Serial.read();  
   btVal = BTSerial.read();
   
-  if (val == '0') {              // if '0' was received led 13 is switched off
-    digitalWrite(ledpin, LOW);    // turn Off pin 13 off
-    delay(1000);                  // waits for a second   
-    Serial.println("13 off");
+  
+  if (val == '0' || btVal == '0') {
+    digitalWrite(ledpin, LOW);
+    delay(400);
+    Serial.println("off");
+    BTSerial.println("off");
   }
-  if (val == '1') {              // if '1' was received led 13 on
-    digitalWrite(ledpin, HIGH);  // turn ON pin 13 on
-    delay(1000);                  // waits for a second
-    Serial.println("13 on");
+  if (val == '1' || btVal == '1') { 
+    digitalWrite(ledpin, HIGH);
+    delay(400);
+    Serial.println("on");
+    BTSerial.println("off");
   }
   
   
-  if (btVal == '0') {              // if '0' was received led 13 is switched off
-    digitalWrite(ledpin, LOW);    // turn Off pin 13 off
-    delay(1000);                  // waits for a second   
-    Serial.println("13 off");
-  }
-  if (btVal == '1') {              // if '1' was received led 13 on
-    digitalWrite(ledpin, HIGH);  // turn ON pin 13 on
-    delay(1000);                  // waits for a second
-    Serial.println("13 on");
-  }
-
-  
-  if (btVal && btVal > 0) {
+  if (btVal && btVal >= 0) {
     Serial.write("new Value:");
     Serial.println(btVal);
   }
-  if (val && val > 0) {
+  if (val && val >= 0) {
     BTSerial.println(val);
   }
 
