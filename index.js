c7d9b55cab825b7bc67c07013ab288745502d8eb
matze <matthias.doetsch@innogames.com>
2017-10-15 18:25:07 +0200

    removed input/output dependency from "control"

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var
-    input   = require('./lib/input'),
-    output  = require('./lib/output'),
-    emitter = require('./lib/input')(),
-    config  = require('./lib/config'),
     control = require('./lib/control'),
     plugins = require('./lib/plugins');
 
-control.loadAll(emitter, output);
-plugins.loadAll(emitter, input, output);
+control.loadAll();
+plugins.loadAll();
 
 require('./lib/server');
